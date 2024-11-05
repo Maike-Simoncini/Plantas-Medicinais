2df7bb123d1c2793ecc407365bc17ba6223d9f95
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2024-11-05 08:42:05 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Projeto de Busca de Plantas Medicinais
+
+**Descrição**
+
+Este projeto web simples permite aos usuários pesquisar por Plantas Medicinais. Ao digitar o nome de um planta no campo de pesquisa e pressionar Enter ou clicar no botão de pesquisa, a aplicação busca por correspondências no nome ou nas tags associadas a cada planta. Os resultados são exibidos em uma lista, com o nome da planta, uma breve descrição e um link para mais informações.
+
+**Tecnologias Utilizadas**
+
+* HTML: Estrutura da página web.
+* CSS: Estilização da página web.
+* JavaScript: Lógica da aplicação, incluindo a função de pesquisa e a manipulação do DOM.
+* Google Fonts: Fontes e ícones personalizados para a página.
+
+**Estrutura do Projeto**
+
+* index.html: Arquivo principal da página web.
+* style.css: Arquivo de estilo para a página.
+* dados.js: Arquivo contendo os dados das plantas (nome, descrição, link e tags).
+* app.js: Arquivo contendo a lógica JavaScript da aplicação.
+
+**Quer testar o projeto?**
+
+É só acessar o link: 
+
---
+++ dados.js
@@ -0,0 +1,74 @@
+let dados = [
+  {
+    nome: "Cannabis",
+    descricao: "Usado no alívio do enjoo e depressão relacionados com o câncer.<br> Reduz significativamente a pressão intraocular e o fluxo lacrimal em pacientes com glaucoma.",
+    link: "https://pt.m.wikipedia.org/wiki/Cannabis",
+    tags: "canabinoides"
+  },
+  {
+    nome: "Alfavaca",
+    descricao: "As folhas frescas ou desidratadas, têm uso culinário.<br> Uso medicinal: Rins, prisão de ventre, aftas, bronquite, gripes fortes.",
+    link: "https://pt.m.wikipedia.org/wiki/Alfavaca",
+    tags: "Ocimum"
+  },
+  {
+    nome: "Barbatimão",
+    descricao: "constitui um género botânico pertencente à família Fabaceae.<br> Uso medicinal: Gastrite, úlceras. Uso externo: Cicatrizante, lavagem íntima.",
+    link: "https://pt.m.wikipedia.org/wiki/Barbatim%C3%A3o",
+    tags: "Stryphnodendron"
+  },
+  {
+    nome: "Funcho",
+    descricao: "O funcho costuma ser utilizado na culinária como tempero e principalmente para produção de chá, já que tem propriedades que auxiliam o intestino e tem ação calmante.<br> Uso medicinal: Gases, digestivo e relaxante.",
+    link: "https://pt.m.wikipedia.org/wiki/Funcho",
+    tags: "Foeniculum"
+  },
+  {
+    nome: "Graviola",
+    descricao: "A gravioleira é uma planta originária das Antilhas. A polpa da fruta consiste em uma polpa branca comestível, algumas fibras e um núcleo de sementes pretas indigeríveis. A polpa também é usada para fazer néctar de frutas, smoothies, sucos de frutas, doces, sorvetes e aromatizantes de sorvete. Uso medicinal: Diabetes, colesterol, emagrecimento.",
+    link: "https://pt.m.wikipedia.org/wiki/Graviola",
+    tags: "Anona muricata"
+  },
+  {
+    nome: "Hibiscus",
+    descricao: "É um gênero de plantas com flor que agrupa cerca de 300 espécies, inserido na família das Malvaceae, com flores e folhas exuberantes. Devido à nova taxonomia pela filogenética (Angiosperm Phylogeny Group), muitas espécies que pertenciam a esse gênero estão migrando para outros gêneros. Uso medicinal: Antifebril, digestivo, relaxante, obesidade.",
+    link: "https://pt.m.wikipedia.org/wiki/Hibiscus",
+    tags: "Hibisco"
+  },
+  {
+    nome: "Dente-de-leão",
+    descricao: "A planta inteira é usada como diurético, laxativo e para facilitar a digestão e estimular o apetite; pode também ser utilizado em casos de obstipação. Além disso, contribui para aumentar a produção de bílis por isso é adequado para os problemas de fígado e vesícula biliar. A raiz é indicada para reumatismo. Faz-se óleo de massagem, também para artrite. Também é conhecida como um ótimo emagrecedor quando tomado (chá) 3 vezes ao dia.",
+    link: "https://pt.wikipedia.org/wiki/Taraxacum",
+    tags: "Taraxacum"
+  },
+  {
+    nome: "Salgueiro",
+    descricao: "A casca do tronco pode ser usada para produção da aspirina; é aliás do nome latino do salgueiro, Salix, que deriva o nome do ácido salicílico e do seu derivado ácido acetilsalicílico.",
+    link: "https://pt.wikipedia.org/wiki/Salgueiro",
+    tags: "Chorão"
+  },
+  {
+    nome: "Arnica",
+    descricao: "A arnica possui as propriedades medicinais devido aos flavonóides, sendo muitos e variados seus usos. Dentre os principais podemos citar: cicatrização de ferimentos superficiais, combate de hemorragias leves, além de ser um ótimo anti-inflamatório natural de uso externo. A arnica não deve ser utilizada por via oral, por ser comprovadamente hepatotóxica.",
+    link: "https://pt.wikipedia.org/wiki/Arnica",
+    tags: "Pele de cordeiro"
+  },
+  {
+    nome: "Guaraná",
+    descricao: "O guaraná é um estimulante: aumenta a resistência nos esforços mentais e musculares e diminui a fadiga motora e psíquica, produz maior rapidez e clareza do pensamento.",
+    link: "https://pt.wikipedia.org/wiki/Guaran%C3%A1",
+    tags: "Paullinia cupana"
+  },
+  {
+    nome: "Canela",
+    descricao: "Canela é uma especiaria obtida a partir da casca interna de várias espécies de árvores do género Cinnamomum (família Lauraceae), usado tanto em alimentos doces como em salgados. Um estudo desenvolvido mostrou que a canela pode diminuir o risco de danos cardiovasculares, ativando moléculas antioxidantes e anti-inflamatórias do corpo.",
+    link: "https://pt.wikipedia.org/wiki/Canela",
+    tags: "Cannella"
+  },
+  {
+    nome: "Hortelã",
+    descricao: "As hortelãs ou mentas são plantas herbáceas vivazes, compreendendo numerosas espécies, das quais muitas são cultivadas em função de suas propriedades aromáticas, condimentares, ornamentais ou medicinais. Em suas propriedades medicinais, é usada como antisséptico, aromática, digestivo, estomáquica e expectorante.",
+    link: "https://pt.wikipedia.org/wiki/Mentha",
+    tags: "Mentha"
+  },
+];
